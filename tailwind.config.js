a7473bf7d6ad20f7043d681846ec5ef78867d8ff
SamuelMorisson <morissonsamuel@gmail.com>
2024-11-18 19:04:18 +0100

    travail partie contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ export default {
     extend: {
       fontFamily: {
         inconsolata: ['Inconsolata']
-      }
+      },
+      boxShadow: {
+        'inner-custom': 'inset 0 4px 4px rgba(0, 0, 0, 0.1)',
+      },
     },
   },
   plugins: [],
