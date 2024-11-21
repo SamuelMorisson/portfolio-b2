14d319a82e196fa36694bd187ff521c12a05a877
SamuelMorisson <morissonsamuel@gmail.com>
2024-11-21 11:08:37 +0100

    création de la partie compétences

--- src/components/Competences.jsx
+++ src/components/Competences.jsx
@@ -1,17 +1,36 @@
 function Competences () {
     return (
         <>
-        <div id="competences" className="my-20 pt-20">
+        <div id="competences" className="flex flex-col items-center my-20 pt-20">
             <div>
-                <h1 className="font-inconsolata text-6xl mb-5">Compétences</h1>
+                <h1 className="font-inconsolata text-6xl mb-9">Compétences</h1>
             </div>
-            <div className="flex flex-col items-center w-full bg-slate-200 rounded-3xl shadow-xl py-3">
-                <ul>
-                    <li>Python</li>
-                    <li>HTML, CSS, JavaScript</li>
-                    <li>React, Vue.js</li>
-                </ul>
-            </div>
+
+            <div className="grid grid-cols-2 place-items-stretch w-2/3">
+                    <div className="flex flex-col items-center bg-slate-200 rounded-3xl shadow-xl py-3 mr-4">
+                        <h2 className="font-inconsolata text-4xl font-semibold text-center mb-3">Front-End</h2>
+                        <div className="grid grid-cols-1 w-1/3">
+                            <ul className="font-inconsolata text-3xl text-left">
+                                <li className="py-2">•HTML/CSS</li>
+                                <li className="py-2">•JavaScript</li>
+                                <li className="py-2">•Vue.js</li>
+                                <li className="py-2">•React</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center bg-slate-200 rounded-3xl shadow-xl py-3 ml-4">
+                        <h2 className="font-inconsolata text-4xl font-semibold text-center mb-3">Back-End</h2>
+                        <div className="grid grid-cols-1 w-1/3">
+                            <ul className="font-inconsolata text-3xl text-left">
+                                <li className="py-2">•Python</li>
+                                <li className="py-2">•Java</li>
+                                <li className="py-2">•C</li>
+                                <li className="py-2">•C++</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
         </div>
         </>
     )
