79cb8e2caf9d1a3ae1e5be2f9a2af219ccfa5bd8
SamuelMorisson <morissonsamuel@gmail.com>
2024-11-22 15:28:03 +0100

    ajout cv dans la présentation

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,17 +1,20 @@
 function Presentation () {
     return (
         <>
-        <div id="presentation" className="my-20 pt-20 grid ">
-            <div className="bg-yellow-200 grid items-center justify-center">
+        <div id="presentation" className="my-20 pt-20 flex flex-col items-center">
+            <div className="items-center justify-center mb-9">
                 <p className="bg-green-200 px-5 text-6xl font-inconsolata">Samuel Morisson</p>
                 <p className="bg-red-200 pl-5 text-4xl text-left font-inconsolata">*étudiant info*</p>
             </div>
-            <div>
-                <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="flex flex-row items-center w-3/5 bg-slate-200 rounded-3xl shadow-xl">
+                <div className="m-5 ml-6 w-4/5">
+                    <p className="border-2 border-slate-300 text-2xl text-center font-inconsolata leading-relaxed">Actuellement en 2e année d`informatique à l`école Ynov, je cherche à développer mes compétences en informatique ainsi qu`avoir une expérience professionnelle en entreprise.</p>
                 </div>
-                <div>
-                    <p>cv ici</p>
+                <div className="bg-green-100 m-5 w-1/5">
+                    <a href="./public/CV Samuel Morisson.pdf" target="_blank" rel="noopener noreferrer">
+                        <img src="./public/screenshot_cv.png" className="border-4 border-slate-400 hover:border-lime-500 hover:shadow-2xl overflow-hidden" />
+                    </a>
+                    <p className="font-inconsolata text-2xl">Mon CV</p>
                 </div>
             </div>
         </div>
