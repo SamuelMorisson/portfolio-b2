3372d116d15ef13fe0112bcac981703082282494
SamuelMorisson <morissonsamuel@gmail.com>
2024-11-25 22:23:58 +0100

    mise à jour de l'affichage des compétences

--- src/components/Competences.jsx
+++ src/components/Competences.jsx
@@ -7,26 +7,48 @@ function Competences () {
             </div>
 
             <div className="grid grid-cols-2 place-items-stretch w-2/3">
-                    <div className="flex flex-col items-center bg-slate-200 rounded-3xl shadow-xl py-3 mr-4">
+                    <div className="flex flex-col items-center bg-slate-200 rounded-3xl border-2 border-lime-500 shadow-xl py-3 mr-4">
                         <h2 className="font-inconsolata text-4xl font-semibold text-center mb-3">Front-End</h2>
-                        <div className="grid grid-cols-1 w-1/3">
-                            <ul className="font-inconsolata text-3xl text-left">
-                                <li className="py-2">•HTML/CSS</li>
-                                <li className="py-2">•JavaScript</li>
-                                <li className="py-2">•Vue.js</li>
-                                <li className="py-2">•React</li>
-                            </ul>
+                        <div className="flex flex-col place-items-center font-inconsolata text-3xl">
+
+                            <div className="flex flex-row items-center w-2/3 my-5">
+                                <img src="./public/logo_webdev.png" className="w-1/3 mx-2" />
+                                <p className="">•HTML/CSS</p>
+                            </div>
+
+                            <div className="flex flex-row items-center w-2/3 my-5">
+                                <img src="./public/logo_javascript.png" className="w-1/3 mx-2" />
+                                <p className="">•JavaScript</p>
+                            </div>
+
+                            <div className="flex flex-row items-center w-2/3 my-5">
+                                <img src="./public/logo_react.png" className="w-1/3 mx-2" />
+                                <p className="">•React</p>
+                            </div>
+
                         </div>
                     </div>
-                    <div className="flex flex-col items-center bg-slate-200 rounded-3xl shadow-xl py-3 ml-4">
+
+
+                    <div className="flex flex-col items-center bg-slate-200 rounded-3xl border-2 border-lime-500 shadow-xl py-3 ml-4">
                         <h2 className="font-inconsolata text-4xl font-semibold text-center mb-3">Back-End</h2>
-                        <div className="grid grid-cols-1 w-1/3">
-                            <ul className="font-inconsolata text-3xl text-left">
-                                <li className="py-2">•Python</li>
-                                <li className="py-2">•Java</li>
-                                <li className="py-2">•C</li>
-                                <li className="py-2">•C++</li>
-                            </ul>
+                        <div className="flex flex-col place-items-center font-inconsolata text-3xl">
+
+                            <div className="flex flex-row items-center w-2/3 my-5">
+                                <img src="./public/logo_python.png" className="w-1/3 mx-2" />
+                                <p className="">•Python</p>
+                            </div>
+
+                            <div className="flex flex-row items-center w-2/3 my-5">
+                                <img src="./public/logo_java.png" className="w-1/3 mx-2" />
+                                <p className="">•Java</p>
+                            </div>
+
+                            <div className="flex flex-row items-center w-2/3 my-5">
+                                <img src="./public/logo_cpp.png" className="w-1/3 mx-2" />
+                                <p className="">•C++</p>
+                            </div>
+
                         </div>
                     </div>
                 </div>
