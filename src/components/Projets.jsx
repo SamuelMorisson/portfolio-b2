5bd720e785339b64750ec2089e6cde73ed814731
SamuelMorisson <morissonsamuel@gmail.com>
2024-11-28 19:05:57 +0100

    test titre Projet

--- src/components/Projets.jsx
+++ src/components/Projets.jsx
@@ -2,12 +2,10 @@ function Projets () {
     return (
         <>
         <div id="projets" className="flex flex-col items-center my-20 pt-20">
-            <div className="flex flex-col items-center md:w-3/4 sm:w-4/5">
+            <div className="flex flex-col items-center md:w-4/5 sm:w-4/5">
                 <div className="text-center mb-9">
-                    <h1 className="font-inconsolata sm:text-6xl minSize:text-5xl p-3 border-2 border-lime-500 rounded-3xl">Projets</h1>
+                    <h1 className="font-inconsolata sm:text-6xl minSize:text-5xl p-3 border-2 border-lime-500 rounded-3xl">Projetsaa</h1>
                 </div>
-
-
 
                 <div className="grid lg:grid-cols-2 md:grid-cols-1 place-items-stretch">
                     <div id="projet1" className="flex flex-col items-center bg-slate-200 rounded-3xl border-2 border-lime-500 shadow-xl py-3 m-2">
