2b0e49b9cf1c9cb27c501068e4401f081617bc7a
SamuelMorisson <morissonsamuel@gmail.com>
2025-03-17 16:58:38 +0100

    ajout d'attributs aria-label

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,19 +11,19 @@ function Burger() {
         <>
         <nav name="navbarDesktop" className="z-20 fixed top-0 left-0 w-full hidden minNavBar:block">
             <div className="grid grid-cols-4 bg-slate-200 rounded-b-3xl mx-3 text-center shadow-xl smallTextNavBar:text-2xl text-xl text-neutral-800 font-inconsolata">
-                <a name="navDPresentation" href="#presentation"
+                <a name="navDPresentation" aria-label="Présentation" href="#presentation"
                     className="bg-slate-200 lg:mx-4 mx-2 my-3 border-2 border-slate-300 hover:border-lime-500 hover:shadow-md  hover:text-black transition-colors duration-200"
                     >Présentation</a>
                 {/*<a href="#experience" 
                     className="bg-slate-200 lg:mx-4 mx-2 my-3 border-2 border-slate-300 hover:border-lime-500 hover:shadow-md hover:text-black transition-colors duration-200"
                     >Expériences</a>*/}
-                <a name="navDCompetences" href="#competences" 
+                <a name="navDCompetences" aria-label="Compétences" href="#competences" 
                     className="bg-slate-200 lg:mx-4 mx-2 my-3 border-2 border-slate-300 hover:border-lime-500 hover:shadow-md hover:text-black transition-colors duration-200"
                     >Compétences</a>
-                <a name="navDProjets" href="#projets" 
+                <a name="navDProjets" aria-label="Projets" href="#projets" 
                     className="bg-slate-200 lg:mx-4 mx-2 my-3 border-2 border-slate-300 hover:border-lime-500 hover:shadow-md hover:text-black transition-colors duration-200"
                     >Projets</a>
-                <a name="navDContact" href="#contact" 
+                <a name="navDContact" aria-label="Contact" href="#contact" 
                     className="bg-slate-200 lg:mx-4 mx-2 my-3 border-2 border-slate-300 hover:border-lime-500 hover:shadow-md hover:text-black transition-colors duration-200"
                     >Contact</a>
             </div>
